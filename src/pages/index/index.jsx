a8dbfa04f29792c2e8077ffc803b771a3ccf8db1
YangWenxiu <wenxiu.yang@31ten.network>
2022-06-17 11:46:25 +0800

    solved

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { View, Picker } from "@tarojs/components";
 import "./index.scss";
 
-// 以下示例中 
+// 以下示例中
 // 将 end / start 取消注释， Picker 将不能使用，详情报错信息请查看控制台-Console
-// taro V3.4.11 或 V3.4.12 都有这个问题 
+// taro V3.4.11 或 V3.4.12 都有这个问题
+
+// 补充：H5 picker mode=date 设置 end/start时， value初始值不能为非日期格式，且取值要在 end-start 之间
 
 export default function Index() {
-  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedDate, setSelectedDate] = useState("2022-06-12");
 
   const pickerChange = e => {
     setSelectedDate(e.detail.value);
@@ -17,7 +19,7 @@ export default function Index() {
     <View className="home-page">
       <Picker
         mode="date"
-        // end={"2022-06-12"}
+        end={"2022-06-12"}
         // start={'1990-06-12'}
         value={selectedDate}
         onChange={pickerChange}
